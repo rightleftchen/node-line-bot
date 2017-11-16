0c3d1630b85bd4a80fec926137aedde841fb63d1
rightleft <rightleft1117@gmail.com>
2017-11-16 21:56:22 +0800

    FOODS Selection

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ let bot = linebot({
 	channelAccessToken: '4muvJafSuXaVcDO3WKUzMrQVUeyLnzK5pBb9As/cn+mGebIDwv7EvaxUpR+Rl+uEmEN4jEEuELt7sqd+id8ecEEa8ruOKp1YlSJJX01F1SUJ0g6MkrauV+0rs8xBtj6AIQmrKhV3KBjcsQUmyLhz2wdB04t89/1O/w1cDnyilFU='
 });
 
+var FOODS=[];
+FOODS[0] = "水餃";
+FOODS[1] = "羊肉";
+FOODS[2] = "蛋包飯";
+FOODS[3] = "滷味";
+FOODS[4] = "香菇雞";
+FOODS[5] = "拉麵";
+FOODS[6] = "牛肉麵";
+FOODS[7] = "一心煎餃";
+FOODS[8] = "酸辣粉";
+FOODS[9] = "咖喱雞肉飯";
+FOODS[10] = "新開咖哩飯";
+FOODS[11] = "燉飯";
+FOODS[12] = "蛋包飯";
 bot.on('message', function(event) {
 	// 把收到訊息的 event 印出來
 	// console.log(event);
@@ -30,6 +44,16 @@ bot.on('message', function(event) {
 				event.reply("Your userId = " + userID).then(function(data) {
 					// sucess
 					console.log("User Id = " + userID);
+				}).catch(function(error) {
+					// error
+					console.log('error');
+				});
+				break;
+			case "吃飯":
+				var option = Math.floor(Math.random()*11);
+				event.reply("你可以吃... " +FOODS[option] + "XDDD" ).then(function(data) {
+					// sucess
+					console.log("\n" + userID + " ： " + msg);
 				}).catch(function(error) {
 					// error
 					console.log('error');

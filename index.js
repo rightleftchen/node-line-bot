e10d0453df03f3398b43afa9f0d90a47d3fdde79
rightleft <rightleft1117@gmail.com>
2017-11-20 13:59:19 +0800

    push message api ----> test2

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
-const line = require('@line/bot-sdk');
-const client = new line.Client({
-	channelAccessToken: '9PcsZfPtX2/LN0QGVNM3HZbKhTnZTE7pXxDf5Fg+GCv3zXdvCPjSf/Ova4HDUDXR6CLR6ZG4IxNsMqYiXJkqKb6fcSwj34+xZZ5ssJOTC04vK9LvpdqPliUjtYWjSWc4MQDJpMxF4YLykOesAS7HbAdB04t89/1O/w1cDnyilFU='
+let linebot = require('linebot'),
+	express = require('express');
+
+const config = require('./config.json'),
+	util = require('util');
+
+let bot = linebot({
+	channelId: config.channelId,
+	channelSecret: config.channelSecret,
+	channelAccessToken: config.channelAccessToken
 });
 
-const message = {
+const line = require('@line/bot-sdk');
+
+const client = new line.Client({
+	channelAccessToken: config.channelAccessToken
+});
+const msg = {
 	type:'text',
-	text:'Hello World!'
+	text:'Elderly Falling Down !!!!'
 };
 
-client.pushMessage('<to>',message)
-  .then(() => {
+var userId = config.clientAppUserID;
 
+client.pushMessage(userId,msg)
+  then(() => {
+  	console.log("Send: " + msg);
   })
   .catch((err) => {
   	// error handling
-  	console.log("Error");
   });
+
+bot.on('message', function(event) {
+	// 把收到訊息的 event 印出來
+	console.log(event);
+});
+
+
+const linebotParser = bot.parser(),
+	app = express();
+app.post('/webhook', linebotParser);
+
+// 在 localhost 走 8080 port
+let server = app.listen(process.env.PORT || 8080,function() {
+	let port = server.address().port;
+	console.log("My Line bot App running on port", port);
+});
+
+
+

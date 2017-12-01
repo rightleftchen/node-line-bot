7855fc9e63234c64a8d3b85390dfdf9495a6d4ff
rightleft <rightleft1117@gmail.com>
2017-12-01 15:16:07 +0800

    Foods options updatr

--- index.js
+++ index.js
@@ -5,34 +5,55 @@ const config = require('./config.json'),
 	util = require('util');
 
 let bot = linebot({
-	channelId: config.channelId,
-	channelSecret: config.channelSecret,
-	channelAccessToken: config.channelAccessToken
+	channelId: '1545688189',
+	channelSecret: '3d9fd88a284afa8b023c8a95ad39425e',
+	channelAccessToken: '4muvJafSuXaVcDO3WKUzMrQVUeyLnzK5pBb9As/cn+mGebIDwv7EvaxUpR+Rl+uEmEN4jEEuELt7sqd+id8ecEEa8ruOKp1YlSJJX01F1SUJ0g6MkrauV+0rs8xBtj6AIQmrKhV3KBjcsQUmyLhz2wdB04t89/1O/w1cDnyilFU='
 });
-
-const line = require('@line/bot-sdk');
-
-const client = new line.Client({
-	channelAccessToken: config.channelAccessToken
-});
-const msg = {
-	type:'text',
-	text:'Elderly Falling Down !!!!'
-};
-
-var userId = config.clientAppUserID;
-
-client.pushMessage(userId,msg)
-  then(() => {
-  	console.log("Send: " + msg);
-  })
-  .catch((err) => {
-  	// error handling
-  });
-
+var FOODS=[];
+FOODS[0] = "水餃";
+FOODS[1] = "羊肉";
+FOODS[2] = "蛋包飯";
+FOODS[3] = "滷味";
+FOODS[4] = "香菇雞";
+FOODS[5] = "拉麵";
+FOODS[6] = "牛肉麵";
+FOODS[7] = "一心煎餃";
+FOODS[8] = "酸辣粉";
+FOODS[9] = "咖喱雞肉飯";
+FOODS[10] = "新開咖哩飯";
+FOODS[11] = "燉飯";
+FOODS[12] = "蛋包飯";
+FOODS[13] = "海鮮粥";
+FOODS[14] = "腿庫飯";
+FOODS[15] = "青菜瘦肉粥";
 bot.on('message', function(event) {
 	// 把收到訊息的 event 印出來
-	console.log(event);
+	// console.log(event);
+	if(event.message.type = 'text') {
+		var msg = event.message.text;
+		var userID = event.source.userId;
+		switch(msg){
+			case "吃飯":
+				var option = Math.floor(Math.random()*15);
+				event.reply("你可以吃... " +FOODS[option] + "XDDD" ).then(function(data) {
+					// sucess
+					console.log("\n" + userID + " ： " + msg);
+				}).catch(function(error) {
+					// error
+					console.log('error');
+				});
+				break;
+			default:
+				event.reply("蛤！你說什麼？").then(function(data) {
+					// sucess
+					console.log(userID + " : " + msg);
+				}).catch(function(error) {
+					// error
+					console.log('error');
+				});
+				break;
+		}
+	}
 });
 
 
@@ -46,5 +67,11 @@ let server = app.listen(process.env.PORT || 8080,function() {
 	console.log("My Line bot App running on port", port);
 });
 
-
-
+// 主動發訊息給 Client App
+setTimeout(function() {
+	var get_userId = config.clientAppUserID;
+	var sendMsg = "push msg to one user";
+	bot.push(get_userId, sendMsg);
+	console.log('userId: ' + get_userId);
+	console.log('send: ' + sendMsg);
+},3000);
